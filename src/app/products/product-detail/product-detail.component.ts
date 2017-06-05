0a7a8f675fb49d62c23d577fad382b8765415354
Justice White <jaxingermany@Justice-Whites-MacBook-Pro.local>
2017-06-04 19:21:34 -0600

    indiv. product details

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+import { Subscription } from 'rxjs/Subscription';
+import { Response } from '@angular/http';
+
+import { UtilityService } from '../../shared/utility.service';
+import { DataStorageService } from '../../shared/data-storage.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.sass']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
+  product: {};
+  productId: string;
 
-  constructor() { }
+  private subscription: Subscription;
+
+  constructor(private route: ActivatedRoute,
+              private utilityService: UtilityService,
+              private dataStorageService: DataStorageService) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      params => {
+        this.productId = params['productId'];
+        this.product = [];
+        this.getProductInfo(this.productId)
+      })
+  }
+
+  getProductInfo(productId: string){
+    this.dataStorageService.getProductById(productId)
+      .subscribe(
+        (product: any[]) => {
+          let tempArr= []
+          tempArr.push(...product)
+          this.product=tempArr[0]
+          this.product = this.utilityService.fixProduct(this.product)
+          console.log(this.product)
+        },(error) => console.log(error)
+      )
+  }
+
+  logStuff(){
+    console.log(this.product)
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
